aad8b3007134bbfc6252fef9b9b27114c8e13702
Urajimiru vorukofu <urajimirudev@gmail.com>
2025-08-09 20:04:48 +0400

    refactor(app): add comments

--- packages/core/App/lib/App.ts
+++ packages/core/App/lib/App.ts
@@ -2,7 +2,16 @@ import { VNode } from '@/core/models/VNode'
 
 import { IApp } from '../models'
 
+/**
+ * Framework application manager.
+ */
 export class App implements IApp {
+  /**
+   * Creates the root virtual node.
+   *
+   * @param node - The root virtual node.
+   * @returns The provided virtual node.
+   */
   create(node: VNode): VNode {
     return node
   }
